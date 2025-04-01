e6a3a3fc81c0d9c3a05a234dcccf89976215f37a
Cyuen0310 <chanszeyuen0310@icloud.com>
2025-04-01 12:35:49 +0800

    Add client and contract as regular directories

---
+++ contract/contracts/Crowdfunding.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
+
+contract CrowdFunding is AutomationCompatibleInterface {
+    struct Campaign{
+        address payable owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 duration;
+        uint256 deadline;
+        uint256 fundedAmount;
+        uint256 numberOfBackers;
+        bool isActive;
+        bool isCollected;
+        mapping(address => uint256) backers; // backers[address] = amount
+    }
+
+    mapping(uint256 => Campaign) public campaigns; // campaigns[id] = campaign
+    mapping (address => uint256[] ) public backedCampaigns; // backedCampaigns[address] = [id, id]
+    uint256 public TotalCampaigns = 0;
+
+    event backedcampaign(uint256 indexed id, address indexed backer, uint256 amount);
+    event createdcampaign (uint256 indexed id, address indexed owner, string title, string description, uint256 target, uint256 duration);
+    event withdrawfund(uint256 indexed id, address indexed owner, uint256 amount);
+
+    function createCampaign(
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _duration
+    ) public returns(uint256){
+        require (_target > 0,"Campaign target must be larger than 0");
+        require (_duration > 0,"Campaign duration must at least be 1 day");
+        uint256 campaignId = TotalCampaigns;
+        Campaign storage campaign = campaigns[campaignId];
+        
+        campaign.owner = payable(msg.sender);
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.duration = _duration;
+        campaign.deadline = block.timestamp + _duration * 1 days;
+        campaign.fundedAmount = 0;
+        campaign.numberOfBackers = 0;
+        campaign.isActive = true;
+        campaign.isCollected = false;
+        TotalCampaigns++;
+        emit createdcampaign(campaignId, msg.sender, _title, _description, _target, _duration);
+        return campaignId;
+    }
+
+    function backCampaign(uint _id) public payable{
+        uint256 fundingValue = msg.value;
+        require (fundingValue > 0,"Funding value must be larger than 0");
+        Campaign storage campaign = campaigns[_id];
+        require (campaign.isActive ,"Campaign is not active");
+        require (campaign.deadline > block.timestamp,"Campaign has ended");
+
+        campaign.fundedAmount += fundingValue;
+        if (campaign.backers[msg.sender] == 0){
+            campaign.numberOfBackers++;
+        }
+        campaign.backers[msg.sender] += fundingValue;
+        backedCampaigns[msg.sender].push(_id);
+
+        emit backedcampaign(_id, msg.sender, fundingValue);
+        
+
+    }
+
+    function checkUpkeep (bytes calldata) external view override returns(bool upkeepNeeded, bytes memory){
+        for (uint256 i = 0; i < TotalCampaigns; i++){
+            Campaign storage campaign = campaigns[i];
+            if (campaign.fundedAmount >= campaign.target || campaign.deadline < block.timestamp){
+                return (true, abi.encode(i));
+            }
+            return (false, "");
+        }
+    }
+
+    function performUpkeep (bytes calldata performData)  external override{
+        uint256 campaignId = abi.decode(performData, (uint256));
+        Campaign storage campaign = campaigns[campaignId];
+        uint256 amount = campaign.fundedAmount;
+        (bool success,) = campaign.owner.call{value: amount}("");
+        require (success, "Transfer failed");
+        campaign.isCollected = true;
+        campaign.isActive = false;
+        emit withdrawfund(campaignId, campaign.owner, amount);
+
+    }
+
+    function getBackedCampaigns(address _backer) public view returns(uint256[] memory){
+        return backedCampaigns[_backer];
+    }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+}
---
+++ contract/scripts/verify/my-contract.js
@@ -0,0 +1,36 @@
+const hre = require("hardhat");
+const dotenv = require("dotenv");
+
+// to run the script:
+//      npx hardhat run scripts/verify/my-contract.js --network zkSyncSepoliaTestnet
+
+async function main() {
+  const contractAddress = process.env.CONTRACT_ADDRESS; // TODO: contract address
+  const constructorArgs = []; // TODO: add constructor params here, if any
+
+  console.log("Verifying contract.");
+  await verify(
+    contractAddress,
+    "contracts/Contract.sol:MyContract",
+    constructorArgs
+  );
+}
+
+async function verify(address, contract, args) {
+  try {
+    return await hre.run("verify:verify", {
+      address: address,
+      contract: contract,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    console.log(address, args, e);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
